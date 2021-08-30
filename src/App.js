0188def63838d4d5b1811fd89394b9598eb3058d
MyeongHoonNam <codenmh0822@naver.com>
2021-08-30 18:21:20 +0900

    feat: App, router Component - route 기능 구현

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import Postspage from "./Component/PostsPage.js";
+import PostEditPage from "./Component/PostEditPage.js";
+import { initRouter } from "./Component/router.js";
+
+export default function App({target}) {
+  const postsPage = new Postspage({target});
+  const postEditPage = new PostEditPage({
+    target,
+    initialState: {
+      postId: 'new',
+      post: {
+        title: '제목 없음',
+        content: '내용 없음',
+      }
+    }
+  });
+
+  this.route = () => {
+    target.innerHTML = '';
+    
+    const {pathname} = location;
+    
+    if(pathname === '/') {
+      postsPage.setState();
+    } else if(pathname.indexOf('/documents/') === 0) {
+      const [, , postId] = pathname.split('/');
+      postsPage.setState();
+      postEditPage.setState({postId});
+    }
+  }
+
+  this.route();
+  initRouter(() => this.route());
+}
