75fdb6a42d39d6dbd76d6fb1836954455b470afc
MyeongHoonNam <codenmh0822@naver.com>
2021-08-30 18:18:10 +0900

    feat: PostsHeader Component

---
+++ src/Component/PostsHeader.js
@@ -0,0 +1,35 @@
+import { route } from "./router.js";
+import {request} from "./api.js"
+
+export default function PostsHeader({target, initialState}) {
+  const postsHeader = document.createElement('div');
+  const postsHeaderText = document.createElement('span');
+  const postsHeaderButton = document.createElement('button');
+
+  this.state = initialState;
+
+  this.render = () => {
+    postsHeader.appendChild(postsHeaderText);
+    postsHeader.appendChild(postsHeaderButton);
+
+    postsHeaderText.innerText = this.state.text;
+    postsHeaderButton.innerText = this.state.button.text;
+
+    target.appendChild(postsHeader);
+  }
+
+  this.render();
+
+  postsHeaderButton.addEventListener('click', async () => {
+    const createdPost = await request(`${this.state.button.link}`, {
+      method: 'Post',
+      body: JSON.stringify({
+        "title": '제목 없음',
+        "parent": null
+      })
+    });
+    console.log(createdPost);
+
+    route(`${this.state.button.link}/${createdPost.id}`);
+  })
+}
