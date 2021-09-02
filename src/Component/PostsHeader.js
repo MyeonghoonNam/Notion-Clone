e1431ec6d0bab06dc2e5d7023be1d818fdf4b6de
MyeongHoonNam <codenmh0822@naver.com>
2021-09-02 16:04:11 +0900

    refactor: 불필요 로직 제거

--- src/Component/PostsHeader.js
+++ src/Component/PostsHeader.js
@@ -1,36 +1,23 @@
-import { route } from "./router.js";
-import {request} from "./api.js"
-
 export default function PostsHeader({target, initialState, onClick}) {
   const postsHeader = document.createElement('div');
   const postsHeaderText = document.createElement('span');
   const postsHeaderButton = document.createElement('button');
 
+  postsHeader.appendChild(postsHeaderText);
+  postsHeader.appendChild(postsHeaderButton);
+  
+  target.appendChild(postsHeader);
+ 
   this.state = initialState;
 
   this.render = () => {
-    postsHeader.appendChild(postsHeaderText);
-    postsHeader.appendChild(postsHeaderButton);
-
-    postsHeaderText.innerText = this.state.text;
-    postsHeaderButton.innerText = this.state.button.text;
-
-    target.appendChild(postsHeader);
-
-    postsHeaderButton.addEventListener('click', (e) => {
-      onClick();
-      // const createdPost = await request(`${this.state.button.link}`, {
-      //   method: 'Post',
-      //   body: JSON.stringify({
-      //     "title": '',
-      //     "parent": null
-      //   })
-      // });
-  
-      // route(`${this.state.button.link}/${createdPost.id}`);
-    })
+    postsHeaderText.innerText = this.state.headerText;
+    postsHeaderButton.innerText = this.state.buttonText;
   }
-
+  
   this.render();
 
+  postsHeaderButton.addEventListener('click', () => {
+    onClick();
+  })
 }
