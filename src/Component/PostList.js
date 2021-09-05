9c22a3efdb31926f31acd5c48da488a1056a9b57
MyeongHoonNam <codenmh0822@naver.com>
2021-09-05 16:12:17 +0900

    style: postList의 기본 패딩값 지정을 통해 리스트 UI 개선

--- src/Component/PostList.js
+++ src/Component/PostList.js
@@ -1,6 +1,13 @@
-import { getItem } from "./storage.js";
+import { getItem } from './storage.js';
 
-export default function PostList({target, initialState, onRemove, onAdd, onPostClick, onToggle}) {
+export default function PostList({
+  target,
+  initialState,
+  onRemove,
+  onAdd,
+  onPostClick,
+  onToggle,
+}) {
   const postList = document.createElement('div');
   postList.setAttribute('class', 'post__list');
 
@@ -8,57 +15,70 @@ export default function PostList({target, initialState, onRemove, onAdd, onPostC
 
   this.state = initialState;
 
-  this.setState = nextState => {
+  this.setState = (nextState) => {
     this.state = nextState;
     this.render();
-  }
+  };
 
   this.render = () => {
-    postList.innerHTML = createdPostList(this.state);
+    postList.innerHTML = createdPostList(this.state, 0); // 상태, 기본 패딩 값
 
     postList.querySelector('ul').addEventListener('click', (e) => {
-      const clickElement = e.target;
-      const {className} = clickElement;
+      const clickElement =
+        e.target.tagName === 'I' ? e.target.closest('button') : e.target;
+      const { className } = clickElement;
       const post = clickElement.closest('li');
       const postId = post.dataset.id;
-  
-      if(post) {
-        if(className === 'post__button--delete') {
+
+      if (post) {
+        if (className === 'post__button--delete') {
           onRemove(postId);
-        } else if(className === 'post__button--add') {
+        } else if (className === 'post__button--add') {
           onAdd(postId);
-        } else if(className.includes("post__button--toggle")) {
+        } else if (className === 'post__button--toggle') {
           onToggle(postId);
         } else {
           onPostClick(postId);
         }
       }
     });
-  }
+  };
 
-  const createdPostList = (documents) => {
+  const createdPostList = (documents, postRowItemPadding) => {
     const toggleIds = getItem('toggleIds', []);
+    postRowItemPadding += 10;
 
     return /* html */ `
       <ul>
-        ${documents.map(({id, title, documents}) => /* html */ `
-          <li class="post__row" data-id="${id}">
-            <div class="post__row__items">
-              <div class="post__row__items--textbox">
-                <button class="post__button--toggle"><i class="fas fa-caret-right"></i></button>
-                <span>${title ? title : '제목 없음'}</span>
-              </div>
-              <div class="post__row__items--buttonbox">
-                <button class="post__button--add"><i class="far fa-plus-square"></i></button>
-                <button class="post__button--delete"><i class="far fa-minus-square"></i></button>
-              </div>
-            </div>
-            ${toggleIds.includes(String(id)) ? documents.length ? createdPostList(documents) : '<ul><li>하위 페이지가 없습니다.</li></ul>' : ''}
-          </li>
-        `).join('')}
+        ${documents
+          .map(
+            ({ id, title, documents }) => /* html */ `
+              <li class="post__row" data-id="${id}">
+                <div class="post__row__items" style="padding-left:${postRowItemPadding}px">
+                  <div class="post__row__items--textbox">
+                    <button class="post__button--toggle"><i class="fas fa-caret-right"></i></button>
+                    <span class="post__text">${
+                      title ? title : '제목 없음'
+                    }</span>
+                  </div>
+                  <div class="post__row__items--buttonbox">
+                    <button class="post__button--add"><i class="far fa-plus-square"></i></button>
+                    <button class="post__button--delete"><i class="far fa-minus-square"></i></button>
+                  </div>
+                </div>
+                ${
+                  toggleIds.includes(String(id))
+                    ? documents.length
+                      ? createdPostList(documents, postRowItemPadding)
+                      : '<ul><li>하위 페이지가 없습니다.</li></ul>'
+                    : ''
+                }
+              </li>`
+          )
+          .join('')}
       </ul>
-    `
+    `;
   };
-  
+
   this.render();
 }
